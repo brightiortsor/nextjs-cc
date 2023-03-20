ad77ce8060f33844022ca56233da1c44f671e537
Bright Iortsor <iortsor@gmail.com>
2023-03-20 11:27:18 +0100

    added routing

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import styles from "../src/styles/Layout.module.css";
+import Nav from "./Nav";
 
 type LayoutProps = {
   children: ReactNode;
@@ -7,11 +8,14 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>MY FIRST NEXTjs PROJECT</h1>
-        {children}
-      </main>
-    </div>
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1>MY FIRST NEXTjs PROJECT</h1>
+          {children}
+        </main>
+      </div>
+    </>
   );
 }
